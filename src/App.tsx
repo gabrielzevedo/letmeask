15f3af08fa006c4147d12b2d3a1c738b17fefb0d
gabrielzevedo <gabrielx86@live.com>
2021-06-22 19:48:41 -0300

    Created component ButtonIncrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Button } from './components/Button'
+import { ButtonIncrement } from './components/ButtonIncrement'
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+        <Button text="Botão 1" />
+        <Button>
+            Botão 2
+        </Button>
+        <ButtonIncrement />
+    </>
   );
 }
 
